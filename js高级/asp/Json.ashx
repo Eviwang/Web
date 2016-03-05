dd4730d3957988673352353d16a82f7392ffd6fe
Evi.Wang <525205537@qq.com>
2016-03-05 11:51:29 +0800

    学习ajax

---
+++ js高级/asp/Json.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Json.ashx.cs" Class="js高级.asp.json" %>
---
+++ js高级/asp/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace js高级.asp
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
